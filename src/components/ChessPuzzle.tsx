d9d4c762f76ab0c03fe2ad47e8a81f103d446dd9
Shivansh Dengla <shivanshdengla@gmail.com>
2025-07-19 18:47:18 +0530

    payments error

--- src/components/ChessPuzzle.tsx
+++ src/components/ChessPuzzle.tsx
@@ -46,6 +46,7 @@ export function ChessPuzzle() {
     setIsLost(false);
   };
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const onDrop = (sourceSquare: Square, targetSquare: Square, _piece: Piece): boolean => {
     if (isSolved || isLost) return false;
 
