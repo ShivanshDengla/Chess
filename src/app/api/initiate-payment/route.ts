1a8fb6792fe08bebc3b94e4100d57213ba2863f0
Shivansh Dengla <shivanshdengla@gmail.com>
2025-07-19 20:33:20 +0530

    changing flow

--- src/app/api/initiate-payment/route.ts
+++ src/app/api/initiate-payment/route.ts
@@ -1,9 +1,22 @@
 import { NextResponse } from 'next/server';
 
-export async function POST() {
-  const uuid = crypto.randomUUID().replace(/-/g, '');
+function generateRandomString(length: number): string {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
 
-  // TODO: Store the ID field in your database so you can verify the payment later
-
-  return NextResponse.json({ id: uuid });
+export function GET() {
+  try {
+    const id = crypto.randomUUID();
+    return NextResponse.json({ id });
+  } catch (error) {
+    console.warn('crypto.randomUUID() is not available. Using a fallback. (This is expected in non-secure contexts)');
+    const id = generateRandomString(36);
+    return NextResponse.json({ id });
+  }
 }
--- src/components/ChessPuzzle.tsx
+++ src/components/ChessPuzzle.tsx
@@ -183,13 +183,13 @@ export function ChessPuzzle() {
       setIsPaying(true);
       // Logic to pay and retry the same puzzle (level remains unchanged)
       const res = await fetch('/api/initiate-payment', {
-        method: 'POST',
+        method: 'GET',
       });
       const { id } = await res.json();
 
       const payload: PayCommandInput = {
         reference: id,
-        to: '0xa057aA66d80ED8be066C8e4261c4b629130679d0',
+        to: '0xe303fffe0221d8f0c6897fec88f8524f7e719fc1',
         tokens: [
           {
             symbol: Tokens.WLD,
