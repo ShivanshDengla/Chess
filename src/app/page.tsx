774d86796400e2256952044c13cc866e87926b48
Shivansh Dengla <shivanshdengla@gmail.com>
2025-07-20 14:45:38 +0530

    loading screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import { Page } from '@/components/PageLayout';
-import { AuthButton } from '../components/AuthButton';
+import { LoadingScreen } from '../components/LoadingScreen';
 
 export default function Home() {
   return (
     <Page>
       <Page.Main className="flex flex-col items-center justify-start pt-4">
-        <AuthButton />
+        <LoadingScreen />
       </Page.Main>
     </Page>
   );
